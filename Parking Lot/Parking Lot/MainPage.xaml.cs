6bd6e890b9a1054ef05f4d922766a5916ba26569
DiegoG <dgst1704@gmail.com>
2024-09-16 23:33:43 -0500

    Agregar archivos de proyecto.

---
+++ Parking Lot/Parking Lot/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Parking_Lot.views;
+
+namespace Parking_Lot
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		// Evento para el botón de iniciar sesión
+		private async void OnLoginButtonClicked(object sender, EventArgs e)
+		{
+			// Navegación a la página de inicio de sesión (Login.xaml en la carpeta Views)
+			await Application.Current.MainPage.Navigation.PushModalAsync(new Login());
+		}
+
+		// Evento para el botón de registrarse
+		private async void OnRegisterButtonClicked(object sender, EventArgs e)
+		{
+			// Lógica para navegar a la página de registro (si existe en Views)
+			await Application.Current.MainPage.Navigation.PushModalAsync(new Registrar());
+		}
+	}
+}
---
+++ Parking Lot/Parking Lot/views/Login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Parking_Lot.views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
+		private async void OnLoginButtonNumber(object sender, EventArgs e)
+		{
+			// Navegación a la página de inicio de sesión (Login.xaml en la carpeta Views)
+			await Application.Current.MainPage.Navigation.PushModalAsync(new Number());
+		}
+	}
+}
